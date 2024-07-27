56d0243b03bcd6ea21a4db03d48085066c6d7b60
IvanDimovSIT <ivan390@abv.bg>
2024-07-27 18:07:01 +0300

    Added persistence for adminEditComment operation

--- core/src/main/java/com/tinqinacademy/comments/core/services/SystemServiceImpl.java
+++ core/src/main/java/com/tinqinacademy/comments/core/services/SystemServiceImpl.java
@@ -5,21 +5,35 @@ import com.tinqinacademy.comments.api.operations.system.admindeletecomment.Admin
 import com.tinqinacademy.comments.api.operations.system.admineditcomment.AdminEditCommentInput;
 import com.tinqinacademy.comments.api.operations.system.admineditcomment.AdminEditCommentOutput;
 import com.tinqinacademy.comments.api.services.SystemService;
+import com.tinqinacademy.comments.core.exception.exceptions.NotFoundException;
+import com.tinqinacademy.comments.persistence.model.Comment;
+import com.tinqinacademy.comments.persistence.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class SystemServiceImpl implements SystemService {
+    private final ConversionService conversionService;
+    private final CommentRepository commentRepository;
+
     @Override
     public AdminEditCommentOutput adminEditComment(AdminEditCommentInput input) {
         log.info("start adminEditComment input:{}", input);
 
-        AdminEditCommentOutput output = AdminEditCommentOutput.builder()
-                .id(input.getCommentId())
-                .build();
+        Comment commentToEdit = commentRepository.findById(UUID.fromString(input.getCommentId()))
+                .orElseThrow(() -> new NotFoundException("Comment with id:"+input.getCommentId()+" not found"));
+        commentToEdit.setContent(input.getContent());
+        //TODO: set lastEditedBy
+        //TODO: set userId
+
+        Comment editedComment = commentRepository.save(commentToEdit);
+        AdminEditCommentOutput output = conversionService.convert(editedComment, AdminEditCommentOutput.class);
 
         log.info("end adminEditComment result:{}", output);
 
