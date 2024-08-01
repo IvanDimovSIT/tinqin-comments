e61b96ac88f5080032f27058cb7710210836d278
IvanDimovSIT <ivan390@abv.bg>
2024-08-01 13:56:21 +0300

    Changed ViolationException to extend RuntimeException

--- core/src/main/java/com/tinqinacademy/comments/core/exception/exceptions/ViolationException.java
+++ core/src/main/java/com/tinqinacademy/comments/core/exception/exceptions/ViolationException.java
@@ -1,17 +1,14 @@
 package com.tinqinacademy.comments.core.exception.exceptions;
 
-import com.tinqinacademy.comments.core.exception.BaseException;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
 @Getter
-public class ViolationException extends BaseException {
+public class ViolationException extends RuntimeException {
     private final List<String> errors;
 
     public ViolationException(List<String> errors) {
-        super("ViolationException", HttpStatus.BAD_REQUEST);
         this.errors = errors;
     }
 }
