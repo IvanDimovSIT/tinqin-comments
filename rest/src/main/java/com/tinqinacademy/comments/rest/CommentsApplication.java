29f60c0e9ec0528fa66adbf9b3b3fd188915a092
IvanDimovSIT <ivan390@abv.bg>
2024-08-06 10:52:12 +0300

    Changed OpenFeign dependency

--- rest/src/main/java/com/tinqinacademy/comments/rest/CommentsApplication.java
+++ rest/src/main/java/com/tinqinacademy/comments/rest/CommentsApplication.java
@@ -3,7 +3,6 @@ package com.tinqinacademy.comments.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.tinqinacademy")
 @EnableJpaRepositories(basePackages = "com.tinqinacademy.comments.persistence")
 @EntityScan(basePackages = "com.tinqinacademy.comments.persistence.model")
-@EnableFeignClients
 public class CommentsApplication {
 
     public static void main(String[] args) {
